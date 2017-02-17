a572db2ace811e5cfbef99ab0cba5c84dc68145a
otofune <otofune@gmail.com>
2017-02-17 01:37:07 +0000

    [posts/show] remove noneed informations

--- src/endpoints/posts/show.js
+++ src/endpoints/posts/show.js
@@ -3,5 +3,10 @@ module.exports = async function (id) {
     var post = await models.posts.findById(id).populate("app user files replyTo repostTo")
     if (!post) throw "post-not-found"
     if (post.user.isSuspended) throw "this-user-is-suspended"
+    // remove noneed informations
+    if (post.repostTo) {
+        delete post.favoriteCount
+        delete post.repostCount
+    }
     return post
 }
